97742fdda0eac649056d8237bb22881d9b5bc85c
Jose Enciso <josejefloen@gmail.com>
2019-12-26 15:39:29 +0000

    JS changes to leave HTML as clean as possible

--- js/band_in_townAPI.js
+++ js/band_in_townAPI.js
@@ -1,11 +1,36 @@
+function fetchBandInformation(event){
 
-    
-function fetchBandInTownInformation(event){
-    $("#band").html("");
-    $("#location").html("");
+    const bitURL = "https://rest.bandsintown.com/artists";
+    const bitAPI = "39605faedc3f5b56c8bd18919d8a9c2a";
+    let artists = $("#artists").val();
+    console.log(artists);
+    console.log(bitAPI);
 
-   $.getJSON(`https://rest.bandsintown.com/artists/${artist}?app_id=39605faedc3f5b56c8bd18919d8a9c2a`)
+    $.getJSON(`${bitURL}/${artists}?app_id=${bitAPI}`,
+    $.getJSON(`${bitURL}/${artists}/events?app_id=${bitAPI}`),
+    /*$.getJSON(`${bitURL}/${artists}/events?app_id=${bitAPI}`),*/
+     function (artistData, artistsEvent) {
+
+        console.log(artistData);
+        console.log(artistsEvent);
+        var bitArtists = artistData;
+        var bitEvents = artistsEvent;
+        console.log(bitArtists);
+        console.log(bitEvents);
+        $("#artistsAlbum").attr("src", artistData.image_url);
+        $("#name").html(artistData.name);
+
+        $("#artistsTable").html(`
+            table  
+            <h2>${artistData.name}</h2>
+            <p>${artistsEvent.country}
+        `);
+        
+    });
+
+}
+
+   
 
 
-
-} 
+$(document).ready(fetchBandInformation);
