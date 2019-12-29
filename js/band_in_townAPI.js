5c1b8a43e2cf3f9bff68e3a5c49d29076bc800f2
Jose Enciso <josejefloen@gmail.com>
2019-12-29 15:57:11 +0000

    Information Artists and Events

--- js/band_in_townAPI.js
+++ js/band_in_townAPI.js
@@ -1,36 +1,57 @@
+function artistInfo(artist){
+    $("#name").html(`${artist.name}`);
+    $("#artistsAlbum").attr("src", `${artist.image_url}`);
+    console.log(artist.name);
+    $(".headRow").show();
+    
+}
+
+
+
 function fetchBandInformation(event){
 
     const bitURL = "https://rest.bandsintown.com/artists";
     const bitAPI = "39605faedc3f5b56c8bd18919d8a9c2a";
     let artists = $("#artists").val();
-    console.log(artists);
-    console.log(bitAPI);
 
-    $.getJSON(`${bitURL}/${artists}?app_id=${bitAPI}`,
-    $.getJSON(`${bitURL}/${artists}/events?app_id=${bitAPI}`),
-    /*$.getJSON(`${bitURL}/${artists}/events?app_id=${bitAPI}`),*/
-     function (artistData, artistsEvent) {
+    $(".headRow").hide();
 
-        console.log(artistData);
-        console.log(artistsEvent);
-        var bitArtists = artistData;
-        var bitEvents = artistsEvent;
-        console.log(bitArtists);
-        console.log(bitEvents);
-        $("#artistsAlbum").attr("src", artistData.image_url);
-        $("#name").html(artistData.name);
+    $.when(
+        $.getJSON(`${bitURL}/${artists}?app_id=${bitAPI}`),
+        $.getJSON(`${bitURL}/${artists}/events?app_id=${bitAPI}`)
+    ).then(
+        function (artResp, eventResp) {
 
-        $("#artistsTable").html(`
-            table  
-            <h2>${artistData.name}</h2>
-            <p>${artistsEvent.country}
-        `);
-        
-    });
+            var artists = artResp[0];
+            var events = eventResp[0];
+            
+            console.dir(artists);
+            console.dir(events);
+            
+            $("#artist-info").html(artistInfo(artists));
+            //$("#eventRepo").html(eventInfo(events));
 
-}
 
-   
+        },
+        function error(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#name").html("");
+                $("#name").html("Artists not found");
+                $("#artistsAlbum").arrt("src", "")
+                
+            } else if (errorResponse.status === 403) {
+                $("#name").html("");
+                $("#name").html("Wait certain time: ");
+            } else {
+                $("#name").html("");
+                $("#name").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                
+            }
+        }
+    );
+
+};
+
 
 
 $(document).ready(fetchBandInformation);
