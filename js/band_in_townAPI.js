432bb2d10918b97be114d023a6115af326222f65
Jose Enciso <josejefloen@gmail.com>
2020-01-09 20:45:50 +0000

    Updating

--- js/band_in_townAPI.js
+++ js/band_in_townAPI.js
@@ -12,16 +12,70 @@ function eventInfo(bandEvent) {
         var venueCountry = bandEvent[index].venue.country;
         var dateEvent = bandEvent[index].datetime;
         var urlEvent = bandEvent[index].url;
-//        console.log((index + 1) + "\t" + venueName + "\t" + venueCity + "\t" + venueCountry + "\t" +dateEvent+ "\t" + urlEvent);
 
+        var lat = bandEvent[index].venue.latitude;
+        var long = bandEvent[index].venue.longitude;
+
+        
+        var options = {
+            zoom: 10,
+            center: {
+                lat: bandEvent[index].venue.latitude,
+                long: bandEvent[index].venue.longitude
+                }
+            };
+//        console.log((index + 1) + "\t" + venueName + "\t" + venueCity + "\t" + venueCountry + "\t" +dateEvent+ "\t" + urlEvent);
+        //var APIKey = "AIzaSyD50Cf8z1w1f_pxvRR1vr-_EPc3yP0Qpnc";
+
+        var map;
+    /*    
+        map = new google.maps.Map(document.getElementById("map"), options);
+        function rederMap() {
+            var options = {
+                zoom: 10,
+                center: {
+                    lat: 40.785091, 
+                    long: -73.968285
+                }
+            };
+        };
+*/
+        
+
+           /* var labels = "ABCDEFGHIJKLMNÑOPQRSTUVXYZ";
+
+            var locations = [
+                { lat: 40.785091, lng: -73.968285 },
+                { lat: 41.084045, lng: -73.874245 },
+                { lat: 40.754932, lng: -73.984016 }
+            ];
+
+            /*map its a JS method and can take up to three arguments and work similar to a for each( ) function*/
+            /*arguments = location is the current value where we are in the array and the "i" is the index number*/
+         /*   var markers = locations.map(function (location, i) {
+                return new google.maps.Marker({*//*Get one of the strings out of the labels we created*/
+                   /* position: location,
+                    label: labels[i % labels.length]
+                  */  /*The reason for using the %operator is so that if we have more than 26 locations, then it will loop around to the start of our string again and go from Z back to A, instead of throwing an error.*/
+                /*});
+            });*//*
+
+        /*    // Add a marker clusterer to manage the markers.
+            var markerCluster = new MarkerClusterer(map, markers,
+                { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+        } */   
+        
         return document.getElementById("showList").innerHTML += `
-        <tr>
+        <tr id="bandInfoRow">
             <td> ${ index + 1} </td>
             <td> ${venueName} </td>
             <td> ${venueCity} </td>
             <td> ${venueCountry} </td>
+            <td id="map" class="location"> <a href="${}"></a> </td>
             <td> ${dateEvent} </td>
             <td> <a href="${urlEvent}"> Get it!</a>  </td>
+            
         </tr>
     `;
     });
@@ -48,6 +102,15 @@ function fetchBandInformation(event) {
     ).then(
         function (artResp, eventResp) {
 
+            console.log(eventResp);
+
+            $("#artists2").autocomplete({
+                source: eventResp[0].name
+                
+            });
+            var artists2 = $("#artists2").val();
+            console.log(artists2);
+
             var artists = artResp[0];
             var events = eventResp[0];
 
@@ -66,9 +129,9 @@ function fetchBandInformation(event) {
                 $("#name").html("");
                 $("#name").html("Wait certain time: ");
             } else {
-                $("#name").html("");
+                /*$("#name").html("");
                 $("#name").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-
+*/
             }
         }
     );
@@ -76,3 +139,18 @@ function fetchBandInformation(event) {
 };
 
 $(document).ready(fetchBandInformation);
+    
+  /*  , function(){
+    $("#artists2").autocomplete({
+        source: `https://rest.bandsintown.com/artists/${$("artists2").val()}?app_id=39605faedc3f5b56c8bd18919d8a9c2a`
+    })
+});
+    
+*/
+
+function renderGoogleMapLink(venue){
+    return 
+    `
+    <a class="googleMapPopUp" rel="nofollow" href="https://maps.google.com.au/maps?q=south+australia" target="_blank">View location map </a>
+    `
+}
